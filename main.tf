2bab1396848e2095aa9f1e248eac9954862c3f97
Franky Parcon <franky.parcon@globe.com.ph>
2026-02-12 08:58:39 +0800

    Added allow all tcp traffic

--- main.tf
+++ main.tf
@@ -78,6 +78,14 @@ resource "aws_security_group" "py_app_sg" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    ingress {
+        description = "Allow all tcp Traffic"
+        from_port = 0
+        to_port = 65535
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         description = "allow all outbound traffic"
         from_port = 0
