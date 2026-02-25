94adff995bef1f1a247108f664eae367ac2d4b28
Franky Parcon <franky.parcon@globe.com.ph>
2026-02-25 13:45:03 +0800

    removed all tcp

--- main.tf
+++ main.tf
@@ -78,13 +78,6 @@ resource "aws_security_group" "py_app_sg" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-    ingress {
-        description = "Allow all tcp Traffic"
-        from_port = 0
-        to_port = 65535
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
 
     egress {
         description = "allow all outbound traffic"
